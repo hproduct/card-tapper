58dc041b094481c1a893ddb863b226e300bcde0d
hai1411 <luongquoccuong@gmail.com>
2022-08-03 21:06:21 +0700

    Updates

---
+++ precache-manifest.13717b2979fc1031e26f8f93411ff216.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "404f77779ce5ed32faa4",
+    "url": "/static/css/main.77c60811.chunk.css"
+  },
+  {
+    "revision": "404f77779ce5ed32faa4",
+    "url": "/static/js/main.404f7777.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "b8fc220914054ddebf48",
+    "url": "/static/js/2.b8fc2209.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "905153e94f8bcd20fc7d4b0434906061",
+    "url": "/static/media/avatar.905153e9.png"
+  },
+  {
+    "revision": "a7e0e82ab4bdd385d990df68ee77f8e0",
+    "url": "/static/media/background.a7e0e82a.png"
+  },
+  {
+    "revision": "d9d27d7458ed6ac7014dd9f9ae68c58c",
+    "url": "/index.html"
+  }
+];
